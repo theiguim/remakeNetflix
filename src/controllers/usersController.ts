5107d793c0269d6e7556bcd4cc0311999ee77f05
theiguim <theiguim@gmail.com>
2024-07-30 21:26:50 -0300

    att user

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,17 +3,53 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userService";
 
 export const usersController = {
-  // GET /users/current/watching
-  watching: async (req: AuthenticatedRequest, res: Response) => {
-    const { id } = req.user!
 
-    try {
-      const watching = await userService.getKeepWatchingList(id)
-      return res.json(watching)
-    } catch (err) {
-      if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
-      }
+    // GET /users/current
+    show: async (req: AuthenticatedRequest, res: Response) => {
+        const currentUser = req.user
+
+        try {
+            return res.json(currentUser)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+    // PUT /users/current
+    update: async (req: AuthenticatedRequest, res: Response) => {
+        const { id } = req.user!
+        const { firstName, lastName, phone, email, birth } = req.body
+
+        try {
+            const updatedUser = await userService.update(id, {
+                firstName,
+                lastName,
+                phone,
+                email,
+                birth
+            })
+
+            return res.json(updatedUser)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+    // GET /users/current/watching
+    watching: async (req: AuthenticatedRequest, res: Response) => {
+        const { id } = req.user!
+
+        try {
+            const watching = await userService.getKeepWatchingList(id)
+            return res.json(watching)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
     }
-  }
 }
--- src/routers.ts
+++ src/routers.ts
@@ -33,7 +33,9 @@ router.delete("/favorites", ensureAuth, favoritesController.delete);
 router.post("/likes", ensureAuth, likesController.save );
 router.delete('/likes', ensureAuth, likesController.delete);
 
+router.get('/users/current', ensureAuth, usersController.show)
 router.get('/users/current/watching', ensureAuth, usersController.watching)
+router.put('/users/current', ensureAuth, usersController.update)
 
 
 export {router};

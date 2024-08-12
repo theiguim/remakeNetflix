39d79be0dfc2d69a5cc2d05d3181c078cb02d319
theiguim <theiguim@gmail.com>
2024-08-12 12:23:14 -0300

    corrigindo array get fav

--- src/models/Index.ts
+++ src/models/Index.ts
@@ -12,16 +12,22 @@ Category.hasMany(Course)
 Category.hasMany(Course, { foreignKey: 'categoryId', as: 'courses' }); 
 
 
+
+
 Course.belongsTo(Category)
-Course.hasMany(Episode)
+// Course.hasMany(Episode)
+Course.hasMany(Episode, { foreignKey: 'courseId', as: 'episodes' }); //adc depois
 Course.belongsToMany(User, { through: Favorite })
 Course.belongsToMany(User, { through: Like })
+
 
 Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 Course.belongsTo(Category, { foreignKey: 'categoryId' });
 
-Episode.belongsTo(Course)
+// Episode.belongsTo(Course)
+Episode.belongsTo(Course, { foreignKey: 'courseId' }); //adc depois
 Episode.belongsToMany(User, { through: WatchTime })
+
 
 Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -2,29 +2,29 @@ import { Favorite } from "../models/Favorite"
 
 export const favoriteService = {
 
-    findByUserId: async(userId: number) => {
-        const favorites = await Favorite.findAll({
-            attributes: [["user_id", "userId"]],
-            where: {userId},
-            include: {
-                association: "Course",
-                attributes: [
-                    "id",
-                    "name",
-                    "synopsis",
-                    ["thumbnail_url", "thumbnailUrl"]
-                ]
-            }
-        })
-        return {
-            userId,
-            courses: [
-                favorites.map(favorite => favorite.Course)
-            ]
-        }
-    },
-    
-	create: async (userId: number, courseId: number) => {
+  findByUserId: async (userId: number) => {
+    const favorites = await Favorite.findAll({
+      attributes: [["user_id", "userId"]],
+      where: { userId },
+      include: {
+        association: "Course",
+        attributes: [
+          "id",
+          "name",
+          "synopsis",
+          ["thumbnail_url", "thumbnailUrl"]
+        ]
+      }
+    });
+
+
+    return {
+      userId,
+      courses: favorites.map(favorite => favorite.Course)
+    }
+  },
+
+  create: async (userId: number, courseId: number) => {
     const favorite = await Favorite.create({
       userId,
       courseId
@@ -38,14 +38,14 @@ export const favoriteService = {
       where: {
         userId,
         courseId
-        }
+      }
     })
   },
 
-  isFavorited: async (userId: number, courseId: number)=>{
+  isFavorited: async (userId: number, courseId: number) => {
     const favorite = await Favorite.findOne({
       where: {
-        userId, 
+        userId,
         courseId
       }
     })
